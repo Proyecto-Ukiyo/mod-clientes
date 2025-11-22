1b7b096d896a6710776632cd73421a8480423774
root <prueba@gmail.com>
2025-11-22 22:32:01 +0100

    microservicios clientes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // Crea la aplicación como un Microservicio (TCP Listener)
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
--- src/modulos/clientes/clientes.controller.ts
+++ src/modulos/clientes/clientes.controller.ts
@@ -7,7 +7,7 @@ export class ClientesController {
   constructor(private readonly clientesService: ClientesService) {}
 
   // Define el patrón de mensaje que este Microservicio reconocerá
-  @MessagePattern({ cmd: 'obtener_datos_cliente' })
+  @MessagePattern({ cmd: 'get_cliente' })
   obtenerDatosCliente(clienteId: string) {
     return this.clientesService.findOne(clienteId);
   }
