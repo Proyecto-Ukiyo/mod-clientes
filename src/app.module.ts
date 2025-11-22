2f58f38693a2e04c7ccde6103ad7b71fed49030d
root <prueba@gmail.com>
2025-11-22 23:04:49 +0100

    microservicios clientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,11 @@ import { Cliente } from './modulos/clientes/entities/cliente.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '194.163.170.169', // IP VPS
-      port: 5432, 
-      username: 'user_clientes',
-      password: 'password_clientes',
-      database: 'bd_clientes',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Cliente],
       synchronize: true,
       autoLoadEntities: true,
